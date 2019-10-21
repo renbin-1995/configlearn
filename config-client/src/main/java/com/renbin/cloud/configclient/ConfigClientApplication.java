b74c0625c2b9679aeab7cc2b79eea7d4317fef0e
renbin <bin.ren@advance.ai>
2019-10-21 11:58:18 +0800

    Initial commit spring cloud config

---
+++ config-client/src/main/java/com/renbin/cloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,34 @@
+package com.renbin.cloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+
+}
